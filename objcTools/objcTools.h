da5f65c5cd130cd0c2a3ea19cc55a42074f248f2
Deng <dxb@buduobushao.com>
2016-10-21 15:03:28 +0800

    Initial Commit

---
+++ objcTools/objcTools.h
@@ -0,0 +1,19 @@
+//
+//  objcTools.h
+//  objcTools
+//
+//  Created by Deng on 10/21/16.
+//  Copyright © 2016 Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcTools.
+FOUNDATION_EXPORT double objcToolsVersionNumber;
+
+//! Project version string for objcTools.
+FOUNDATION_EXPORT const unsigned char objcToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcTools/PublicHeader.h>
+
+
